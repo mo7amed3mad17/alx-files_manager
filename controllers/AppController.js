9b66d4b50f3b62e59d931e6516b5a5502fad984e
mo7amed3mad17 <memo12317emad@gmail.com>
2024-12-11 13:48:46 +0200

    2

---
+++ controllers/AppController.js
@@ -0,0 +1,25 @@
+import redisClient from '../utils/redis.js';
+import dbClient from '../utils/db.js';
+
+class AppController {
+  static getStatus(req, res) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    res.status(200).json(status);
+  }
+
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+    const stats = {
+      users: usersCount,
+      files: filesCount,
+    };
+    res.status(200).json(stats);
+  }
+}
+
+export default AppController;
+
